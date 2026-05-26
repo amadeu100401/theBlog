8b08bf9ef04a1c1509a665832511d7ad5b102244
amadeu100401 <amadeu.martim@souunit.com.br>
2026-05-26 12:29:04 -0300

    feature: creating header component and fix page layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "The Blog",
-  description: "Este é um blog com Next.JS",
+  title: 'The Blog',
+  description: 'Este é um blog com Next.JS',
 };
 
 export default function RootLayout({
@@ -23,11 +12,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="pt-BR"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    // Tema claro e escuro
+    <html lang='pt-BR' className='light'>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 import { Suspense } from 'react';
 import { PostsList } from './components/PostLists';
 import { SpinLoader } from './components/SpinLoader';
+import { Container } from './components/Container';
+import { Header } from './components/Header';
 
 export default async function Home() {
   return (
-    <div>
-      <header>
-        <h1 className='text-6xl font-bold text-center py-8'>Aqui é a HEADER</h1>
-      </header>
+    <Container>
+      <Header />
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
       <footer>
         <h1 className='text-6xl font-bold text-center py-8'>Aqui é a FOOTER</h1>
       </footer>
-    </div>
+    </Container>
   );
 }
