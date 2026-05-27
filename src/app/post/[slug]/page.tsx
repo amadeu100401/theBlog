ed90490631609f21824fd0cbaccc7cc8d1717339
amadeu100401 <amadeu.martim@souunit.com.br>
2026-05-26 21:19:55 -0300

    feature: adding dynamic title to post page

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,9 +1,25 @@
 import { findBySlugCached } from '@/lib/post/queries';
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
 type postSlugPageProps = {
   params: Promise<{ slug: string }>;
 };
+
+export async function generateMetadata({
+  params,
+}: postSlugPageProps): Promise<Metadata> {
+  const { slug } = await params;
+
+  const post = await findBySlugCached(slug).catch(() => undefined);
+
+  if (!post) notFound();
+
+  return {
+    title: post.title,
+    description: post.excerpt,
+  };
+}
 
 export default async function PostSlugPage({ params }: postSlugPageProps) {
   const { slug } = await params;
