ecb6453013a2ab57993361da30a6604a1c012589
amadeu100401 <amadeu.martim@souunit.com.br>
2026-06-03 19:14:40 -0300

    feature: creating nav menu

--- src/components/DefaultButton/index.tsx
+++ src/components/DefaultButton/index.tsx
@@ -12,12 +12,18 @@ export function ButtonComponent({
   text,
   ...props
 }: ButtonComponentProps) {
-  const commonStyle =
-    'felx items-center justify-center py-2 px-4 cursor-pointer rounded-lg disabled:bg-slate-200 disabled:text-slate-400 disabled:cursor-default';
+  const commonStyle = clsx(
+    'felx items-center justify-center py-2 px-4',
+    'cursor-pointer rounded-lg',
+    'disabled:bg-slate-200 disabled:text-slate-400 disabled:cursor-default',
+  );
 
   const style = {
-    confirm: 'bg-blue-500 text-blue-50 hover:bg-blue-600 transition',
-    cancel: 'bg-slate-300 text-slate-950 hover:bg-slate-400 transition',
+    confirm: clsx('bg-blue-500 text-blue-50', 'hover:bg-blue-600 transition'),
+    cancel: clsx(
+      'bg-slate-300 text-slate-950',
+      'hover:bg-slate-400 transition',
+    ),
     default: '',
   };
 
